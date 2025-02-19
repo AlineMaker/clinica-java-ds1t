4768804a1a4b2574f928152ea247745c2ceefeed
ALINE ALVES DE SOUZA <25132561@CFP127ALUNO.LOCAL>
2025-02-19 16:54:31 -0300

    Classe Paciente Iniciada.

---
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -0,0 +1,53 @@
+package br.senai.sp.jandira.clinica;
+
+import java.time.LocalDate;
+
+import br.senai.sp.jandira.clinica.model.Paciente;
+
+public class Clinica {
+
+	public static void main(String[] args) {
+		
+		Paciente p1 = new Paciente();
+		p1.nome = "Ana Maria";
+		p1.altura = 1.65;
+		p1.peso = 68.200;
+		p1.telefone = "(11)98888-8888";
+		p1.dataNascimento = LocalDate.of(2003, 7, 15);
+		p1.estaInternado = true;
+				
+		
+		
+		
+		
+		
+		Paciente p2 = new Paciente();
+		p2.nome = "Aline Alves de Souza";
+		p2.altura = 1.50;
+		p2.peso = 106.400;
+		p2.telefone = "(11)97148-2863";
+		p2.dataNascimento = LocalDate.of(1995, 12, 18);
+		p2.estaInternado = false;
+		
+		
+		
+		// Mostrar dados dos pacientes
+		
+		p2.mostrarFichaPaciente();
+	
+	
+	
+		/*
+		 * Paciente p3 = p1;
+		 
+		programador empresa data
+		int idade = 25;
+		System.out.println("Eu tenho " + idade + "anos de idade.");
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3); */
+		
+		
+		
+	}
+}	
