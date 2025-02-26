7310497a6cb271d958f0a1d7b8790c84e45831aa
ALINE ALVES DE SOUZA <25132561@CFP127ALUNO.LOCAL>
2025-02-26 16:04:37 -0300

    Métodos de acesso set do peso e altura implementados

--- src/br/senai/sp/jandira/clinica/Clinica.java
+++ src/br/senai/sp/jandira/clinica/Clinica.java
@@ -10,10 +10,10 @@ public class Clinica {
 		
 		Paciente p1 = new Paciente();
 		p1.nome = "Ana Maria";
-		p1.altura = 1.65;
-		p1.peso = 68.200;
+		p1.setAltura(1.65);
+		p1.setPeso(59.9);
 		p1.telefone = "(11)98888-8888";
-		p1.dataNascimento = LocalDate.of(2003, 7, 15);
+		p1.dataNascimento = LocalDate.of(2003, 2, 28);
 		p1.estaInternado = true;
 				
 		
@@ -23,8 +23,8 @@ public class Clinica {
 		
 		Paciente p2 = new Paciente();
 		p2.nome = "Aline Alves de Souza";
-		p2.altura = 1.50;
-		p2.peso = 106.400;
+		p2.setAltura(1.78);
+		p2.setPeso(41.0);
 		p2.telefone = "(11)97148-2863";
 		p2.dataNascimento = LocalDate.of(1995, 12, 18);
 		p2.estaInternado = false;
@@ -32,7 +32,7 @@ public class Clinica {
 		
 		
 		// Mostrar dados dos pacientes
-		
+		p1.mostrarFichaPaciente();
 		p2.mostrarFichaPaciente();
 	
 	
--- src/br/senai/sp/jandira/clinica/model/Paciente.java
+++ src/br/senai/sp/jandira/clinica/model/Paciente.java
@@ -6,12 +6,30 @@ public class Paciente {
 	
 	public String nome;
 	public LocalDate dataNascimento;
-	public double peso;
-	public double altura;
+	private double peso;
+	private double altura;
 	public String telefone;
 	public boolean estaInternado ;
 	
-
+	public void setAltura(double altura) {
+		if(altura > 0) {
+			this.altura = altura;
+		}else {
+			System.out.println("Altura do(a) paciente " + nome + " está incorreto");
+			
+		}
+		
+		
+}
+	
+	public void setPeso(double peso) {
+			if(peso >= 40.0) {
+				this.peso = peso;			
+			}else {
+				System.out.println("Peso do paciente " + nome + " está incorreto");
+			}
+}
+	
 	public void calcularIdade() {
 		
 	}
